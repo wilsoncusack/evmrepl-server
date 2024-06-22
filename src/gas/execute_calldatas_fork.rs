1a7a80e387f10f887904543c8f8d1c350a622943
Wilson Cusack <wilson.cusack@coinbase.com>
2024-06-22 19:34:45 -0400

    working

--- src/gas/execute_calldatas_fork.rs
+++ src/gas/execute_calldatas_fork.rs
@@ -5,10 +5,11 @@ use alloy_eips::BlockId;
 use alloy_json_abi::Function;
 use alloy_network::Ethereum;
 use alloy_primitives::{Address, Bytes, U256};
+use alloy_rpc_types_eth::BlockTransactionsKind;
 use alloy_transport_http::{Client, Http};
 use forge::{
     backend,
-    executors::{self, CallResult, Executor, ExecutorBuilder, TracingExecutor},
+    executors::{self, CallResult, Executor, ExecutorBuilder, RawCallResult, TracingExecutor},
     fork::{CreateFork, MultiFork},
     inspectors::InspectorStack,
     opts::EvmOpts,
@@ -29,27 +30,47 @@ use std::{str::FromStr, sync::Arc};
 pub async fn execute_calldatas_fork(
     bytecode: Bytes,
     from: Address,
-    func: &Function,
-    args: &[DynSolValue],
+    calldata: Bytes,
     value: U256,
-) -> Result<CallResult, eyre::Error> {
+) -> Result<RawCallResult, eyre::Error> {
     let rpc_url = "https://mainnet.base.org".parse()?;
 
     // Create a provider with the HTTP transport using the `reqwest` crate.
     let provider = ProviderBuilder::new().on_http(rpc_url);
 
     // Get latest block number.
-    let latest_block_number = provider.get_block_number().await?;
-    let latest_block = provider.get_block(id, kind)
+    // let latest_block_number = provider.get_block_number().await?;
+    // let latest_block = provider.get_block(BlockId::latest(), BlockTransactionKind);
 
-    let cfg = CfgEnv::default().with_chain_id(8453);
-    let block = BlockEnv {
-        number: U256::from_str("1234").unwrap(),
+    let (fork_gas_price, rpc_chain_id, block) = tokio::try_join!(
+        provider.get_gas_price(),
+        provider.get_chain_id(),
+        provider.get_block(BlockId::latest(), BlockTransactionsKind::Hashes)
+    )?;
+    let cfg = CfgEnv::default().with_chain_id(rpc_chain_id);
+    let block = if let Some(block) = block {
+        block
+    } else {
+        Err(eyre::eyre!("block not found"))?
+    };
+    let block_env = BlockEnv {
+        number: U256::from(block.header.number.expect("block number not found")),
+        timestamp: U256::from(block.header.timestamp),
+        coinbase: block.header.miner,
+        difficulty: block.header.difficulty,
+        prevrandao: Some(block.header.mix_hash.unwrap_or_default()),
+        basefee: U256::from(block.header.base_fee_per_gas.unwrap_or_default()),
+        gas_limit: U256::from(block.header.gas_limit),
         ..Default::default()
     };
     let env = Env {
         cfg,
-        block,
+        block: block_env,
+        tx: TxEnv {
+            chain_id: Some(rpc_chain_id),
+            gas_limit: block.header.gas_limit as u64,
+            ..Default::default()
+        },
         ..Default::default()
     };
     let env_with_handler = EnvWithHandlerCfg::new_with_spec_id(Box::new(env), SpecId::LATEST);
@@ -62,10 +83,10 @@ pub async fn execute_calldatas_fork(
         b,
         env_with_handler,
         InspectorStack::default(),
-        U256::from_str("2000000").unwrap(),
+        U256::from(block.header.gas_limit),
     );
     let res = e.deploy(Address::ZERO, bytecode, U256::ZERO, None)?;
-    let t = e.transact(from, res.address, func, args, value, None)?;
+    let t = e.transact_raw(from, res.address, calldata, value)?;
 
     Ok(t)
 }
@@ -81,6 +102,7 @@ mod test {
     use alloy_json_abi::JsonAbi;
     use alloy_primitives::{address, hex};
     use alloy_sol_types::sol;
+    use alloy_sol_types::SolCall;
 
     #[tokio::test(flavor = "multi_thread")]
     async fn test_execute() {
@@ -95,15 +117,23 @@ mod test {
             }
         "#;
 
+        sol! {
+            function test(uint256 tokenId) external returns (bytes memory);
+        }
+
         let result = compile::solidity::compile(solidity_code);
         let (json_abi, bytecode) = result.unwrap();
         let abi: JsonAbi = serde_json::from_str(&json_abi).unwrap();
         let f = abi.function("test").unwrap().first().unwrap();
-        let data = hex!("0000000000000000000000000000000000000000000000000000000000000429");
-        let args = DynSolType::Uint(256).abi_decode(&data).unwrap();
+        // let data = hex!("0000000000000000000000000000000000000000000000000000000000000429");
+        let calldata = testCall {
+            tokenId: U256::from(1065),
+        }
+        .abi_encode();
+        // let args = DynSolType::Uint(256).abi_decode(&data).unwrap();
         // let to = address!("cB28749c24AF4797808364D71d71539bc01E76d4");
         let code = Bytes::from_hex(bytecode).expect("error getting bytes");
-        let res = execute_calldatas_fork(code, Address::ZERO, f, &[args], U256::ZERO).await;
-        println!("{:?}", res.unwrap().raw.out);
+        let res = execute_calldatas_fork(code, Address::ZERO, calldata.into(), U256::ZERO).await;
+        println!("{:?}", res.unwrap().out);
     }
 }
