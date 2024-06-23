d063e23bf1f53a29e6c43cd7ebfc41b55ded8117
Wilson Cusack <wilson.cusack@coinbase.com>
2024-06-22 20:05:43 -0400

    added API

--- src/gas/execute_calldatas_fork.rs
+++ src/gas/execute_calldatas_fork.rs
@@ -1,48 +1,36 @@
-use super::Call;
 use alloy::providers::{Provider, ProviderBuilder};
-use alloy_dyn_abi::DynSolValue;
 use alloy_eips::BlockId;
-use alloy_json_abi::Function;
-use alloy_network::Ethereum;
-use alloy_primitives::{Address, Bytes, U256};
+use alloy_primitives::{Address, Bytes, Log, U256};
 use alloy_rpc_types_eth::BlockTransactionsKind;
-use alloy_transport_http::{Client, Http};
-use forge::{
-    backend,
-    executors::{self, CallResult, Executor, ExecutorBuilder, RawCallResult, TracingExecutor},
-    fork::{CreateFork, MultiFork},
-    inspectors::InspectorStack,
-    opts::EvmOpts,
-};
+use forge::{backend, executors::Executor, inspectors::InspectorStack, opts::EvmOpts};
 use foundry_config::Config;
-// use forge::config::{
-//     ethers_solc::{artifacts::{bytecode, Evm}, EvmVersion},
-//     find_project_root_path, Config,
-// };
-use revm::{
-    db::CacheDB,
-    primitives::{Bytecode, ExecutionResult, TransactTo, TxEnv},
-    InMemoryDB, Inspector,
-};
+use revm::{interpreter::InstructionResult, primitives::TxEnv};
 use revm_primitives::{BlockEnv, CfgEnv, Env, EnvWithHandlerCfg, SpecId};
-use std::{str::FromStr, sync::Arc};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Clone)]
+pub struct Call {
+    pub calldata: Bytes,
+    pub value: U256,
+    pub caller: Address,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct ExecutionResult {
+    pub exit_reason: InstructionResult,
+    pub reverted: bool,
+    pub result: Bytes,
+    pub gas_used: u64,
+    pub logs: Vec<Log>,
+}
 
 pub async fn execute_calldatas_fork(
     bytecode: Bytes,
-    from: Address,
-    calldata: Bytes,
-    value: U256,
-) -> Result<RawCallResult, eyre::Error> {
+    calls: Vec<Call>,
+) -> Result<Vec<ExecutionResult>, eyre::Error> {
     let rpc_url = "https://mainnet.base.org".parse()?;
-
-    // Create a provider with the HTTP transport using the `reqwest` crate.
     let provider = ProviderBuilder::new().on_http(rpc_url);
-
-    // Get latest block number.
-    // let latest_block_number = provider.get_block_number().await?;
-    // let latest_block = provider.get_block(BlockId::latest(), BlockTransactionKind);
-
-    let (fork_gas_price, rpc_chain_id, block) = tokio::try_join!(
+    let (_fork_gas_price, rpc_chain_id, block) = tokio::try_join!(
         provider.get_gas_price(),
         provider.get_chain_id(),
         provider.get_block(BlockId::latest(), BlockTransactionsKind::Hashes)
@@ -78,29 +66,36 @@ pub async fn execute_calldatas_fork(
         fork_url: Some("https://mainnet.base.org".into()),
         ..Default::default()
     };
-    let b = backend::Backend::spawn(opts.get_fork(&Config::default(), opts.evm_env().await?));
-    let mut e = executors::Executor::new(
-        b,
+    let backend = backend::Backend::spawn(opts.get_fork(&Config::default(), opts.evm_env().await?));
+    let mut executor = Executor::new(
+        backend,
         env_with_handler,
         InspectorStack::default(),
         U256::from(block.header.gas_limit),
     );
-    let res = e.deploy(Address::ZERO, bytecode, U256::ZERO, None)?;
-    let t = e.transact_raw(from, res.address, calldata, value)?;
+    let res = executor.deploy(Address::ZERO, bytecode, U256::ZERO, None)?;
 
-    Ok(t)
+    calls
+        .into_iter()
+        .map(|call| {
+            let r = executor.transact_raw(call.caller, res.address, call.calldata, call.value)?;
+            Ok(ExecutionResult {
+                exit_reason: r.exit_reason,
+                reverted: r.reverted,
+                result: r.result,
+                gas_used: r.gas_used,
+                logs: r.logs,
+            })
+        })
+        .collect()
 }
 
 #[cfg(test)]
 mod test {
-
-    use crate::compile;
-
     use super::*;
+    use crate::compile;
     use alloy::hex::FromHex;
-    use alloy_dyn_abi::DynSolType;
     use alloy_json_abi::JsonAbi;
-    use alloy_primitives::{address, hex};
     use alloy_sol_types::sol;
     use alloy_sol_types::SolCall;
 
@@ -109,10 +104,10 @@ mod test {
         let solidity_code = r#"
             pragma solidity ^0.8.0;
             contract Test {
-                function test(uint256 tokenId) external returns (bytes memory) {
+                function test(uint256 tokenId) external view returns (bytes memory) {
                     bytes memory c = abi.encodeWithSelector(bytes4(keccak256("ownerOf(uint256)")), tokenId);
-                    (, bytes memory res) = address(0xcB28749c24AF4797808364D71d71539bc01E76d4).call(c);
-                    return abi.encode(block.number);
+                    (, bytes memory res) = address(0xcB28749c24AF4797808364D71d71539bc01E76d4).staticcall(c);
+                    return res;
                 }
             }
         "#;
@@ -133,7 +128,15 @@ mod test {
         // let args = DynSolType::Uint(256).abi_decode(&data).unwrap();
         // let to = address!("cB28749c24AF4797808364D71d71539bc01E76d4");
         let code = Bytes::from_hex(bytecode).expect("error getting bytes");
-        let res = execute_calldatas_fork(code, Address::ZERO, calldata.into(), U256::ZERO).await;
-        println!("{:?}", res.unwrap().out);
+        let res = execute_calldatas_fork(
+            code,
+            vec![Call {
+                caller: Address::ZERO,
+                calldata: calldata.into(),
+                value: U256::ZERO,
+            }],
+        )
+        .await;
+        println!("{:?}", res.unwrap().first().unwrap().result);
     }
 }
--- src/gas/mod.rs
+++ src/gas/mod.rs
@@ -5,3 +5,4 @@ use transact::transact;
 mod execute_calldatas;
 mod execute_calldatas_fork;
 pub use execute_calldatas::{execute_calldatas, Call};
+pub use execute_calldatas_fork::{execute_calldatas_fork, Call as ForkCall, ExecutionResult};
