a2664fe29da973ff76492d9d46ae55dbab2075f7
Wilson Cusack <wilson.cusack@coinbase.com>
2024-08-01 20:06:57 -0400

    not working, checkin

--- src/gas/execute_calldatas_fork.rs
+++ src/gas/execute_calldatas_fork.rs
@@ -5,11 +5,13 @@ use alloy::providers::{Provider, ProviderBuilder};
 use alloy_eips::BlockId;
 use alloy_primitives::{Address, Bytes, Log, U256};
 use alloy_rpc_types_eth::BlockTransactionsKind;
-use forge::{backend, executors::ExecutorBuilder, opts::EvmOpts, traces::CallTraceArena};
+use forge::{backend::{self, DatabaseExt}, executors::ExecutorBuilder, opts::EvmOpts, traces::CallTraceArena};
 use foundry_config::Config;
 use revm::{interpreter::InstructionResult, primitives::TxEnv, InnerEvmContext};
 use revm_primitives::{BlockEnv, Bytecode, CfgEnv, Env};
 use serde::{Deserialize, Serialize};
+
+use crate::gas::transact;
 
 #[derive(Deserialize, Clone)]
 pub struct Call {
@@ -18,15 +20,9 @@ pub struct Call {
     pub caller: Address,
 }
 
+
 #[derive(Deserialize, Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
-
-pub struct ExecuteResponse {
-    pub address: Address,
-    pub results: Vec<ExecutionResult>,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
 pub struct ExecutionResult {
     pub exit_reason: InstructionResult,
     pub reverted: bool,
@@ -40,7 +36,7 @@ pub async fn execute_calldatas_fork(
     bytecode: Bytes,
     address: Address,
     calls: Vec<Call>,
-) -> Result<ExecuteResponse, eyre::Error> {
+) -> Result<Vec<ExecutionResult>, eyre::Error> {
     dotenv().ok();
     let rpc =
         env::var("BASE_RPC").map_err(|_| eyre::eyre!("BASE_RPC environment variable not set"))?;
@@ -86,14 +82,17 @@ pub async fn execute_calldatas_fork(
         .inspectors(|stack| stack.trace_mode(forge::traces::TraceMode::Call).logs(true))
         .build(env, backend);
 
-    executor.deploy(Address::ZERO, bytecode.clone(), U256::ZERO, None)?;
     let db = executor.backend_mut();
     let mut i = InnerEvmContext::new(db);
     i.load_account(address)?;
     i.journaled_state
         .set_code(address, Bytecode::new_raw(bytecode.clone()));
 
-    let results = calls.into_iter().map(|call| {
+    println!("YOOOO {:?}",i.journaled_state.account(address).info);
+    println!("HEYYY {:?}", executor.is_empty_code(address)?);
+
+
+    calls.into_iter().map(|call| {
         let r = executor.transact_raw(call.caller, address, call.calldata, call.value)?;
         Ok(ExecutionResult {
             exit_reason: r.exit_reason,
@@ -103,12 +102,7 @@ pub async fn execute_calldatas_fork(
             logs: r.logs,
             traces: r.traces.unwrap_or(CallTraceArena::default()),
         })
-    });
-
-    Ok(ExecuteResponse {
-        address: address,
-        results: results.collect::<Result<Vec<_>, eyre::Error>>()?,
-    })
+    }).collect()
 }
 
 #[cfg(test)]
@@ -135,13 +129,13 @@ mod tests {
         };
 
         // Execute the call
-        let response = execute_calldatas_fork(bytecode, address, vec![test_call])
+        let results = execute_calldatas_fork(bytecode, address, vec![test_call])
             .await
             .unwrap();
-        println!("{:?}", response);
+        println!("{:?}", results);
         // Assertions
-        assert_eq!(response.results.len(), 1);
-        let execution_result = &response.results[0];
+        assert_eq!(results.len(), 1);
+        let execution_result = &results[0];
 
         // Check that the call was successful
         assert_eq!(execution_result.exit_reason, InstructionResult::Return);
@@ -158,7 +152,7 @@ mod tests {
     async fn test_simple_storage_contract() {
         // Simple storage contract bytecode
         let bytecode = Bytes::from_str("0x608060405234801561001057600080fd5b50610150806100206000396000f3fe608060405234801561001057600080fd5b50600436106100365760003560e01c80632e64cec11461003b5780636057361d14610059575b600080fd5b610043610075565b60405161005091906100d9565b60405180910390f35b610073600480360381019061006e919061009d565b61007e565b005b60008054905090565b8060008190555050565b60008135905061009781610103565b92915050565b6000602082840312156100b3576100b26100fe565b5b60006100c184828501610088565b91505092915050565b6100d3816100f4565b82525050565b60006020820190506100ee60008301846100ca565b92915050565b6000819050919050565b600080fd5b61010c816100f4565b811461011757600080fd5b5056fea2646970667358221220404e37f487a89a932dca5e77faaf6ca2de3b991f93d230604b1b8daaef64766264736f6c63430008070033").unwrap();
-        let address = Address::from_str("0x8be503bcded90ed42eff31f56199399b2b0154ca").unwrap();
+        let address = Address::from_str("0xb2f9974c62815d3177079e150377915d9bc49c82").unwrap();
 
         // Call to store a value
         let store_call = Call {
@@ -178,11 +172,11 @@ mod tests {
         };
 
         // Execute the calls
-        let response = execute_calldatas_fork(bytecode, address, vec![store_call, retrieve_call])
+        let results = execute_calldatas_fork(bytecode, address, vec![store_call, retrieve_call])
             .await
             .unwrap();
 
-        for (i, result) in response.results.iter().enumerate() {
+        for (i, result) in results.iter().enumerate() {
             println!("Call {}", i);
             println!("Result data: 0x{}", hex::encode(&result.result));
             println!("Gas used: {}", result.gas_used);
@@ -193,7 +187,7 @@ mod tests {
 
         // Check the retrieve call result
         assert_eq!(
-            hex::encode(&response.results[1].result),
+            hex::encode(&results[1].result),
             "0000000000000000000000000000000000000000000000000000000000000042"
         );
     }
--- src/gas/mod.rs
+++ src/gas/mod.rs
@@ -5,4 +5,4 @@ pub use transact::transact;
 mod execute_calldatas;
 mod execute_calldatas_fork;
 pub use execute_calldatas::{execute_calldatas, Call};
-pub use execute_calldatas_fork::{execute_calldatas_fork, Call as ForkCall, ExecuteResponse};
+pub use execute_calldatas_fork::{execute_calldatas_fork, Call as ForkCall, ExecutionResult};
